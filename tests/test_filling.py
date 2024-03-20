93010d437db02954fda6def75f72f92080ca45cc
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-20 23:29:31 +0600

    test: added 'filling' module tests with empty images

--- tests/test_filling.py
+++ tests/test_filling.py
@@ -37,6 +37,11 @@ def test_flood_fill_binary_gray():
         flood_fill_binary(img, (2, 2))
 
 
+def test_flood_fill_binary_empty():
+    img = np.empty((0,), dtype=np.uint8)
+    assert (flood_fill_binary(img, (2, 2)) == img).all()
+
+
 def test_darken_areas_near_borders_whole_black():
     img = np.zeros((5, 5), dtype=np.uint8)
     assert (darken_areas_near_borders(img) == img).all()
@@ -54,6 +59,11 @@ def test_darken_areas_near_borders_gray():
         darken_areas_near_borders(img)
 
 
+def test_darken_areas_near_borders_empty():
+    img = np.empty((0,), dtype=np.uint8)
+    assert (darken_areas_near_borders(img) == img).all()
+
+
 def test_brighten_areas_near_borders_whole_black():
     img = np.zeros((5, 5), dtype=np.uint8)
     assert (brighten_areas_near_borders(img) == np.full_like(img, fill_value=255)).all()
@@ -69,3 +79,8 @@ def test_brighten_areas_near_borders_gray():
     img[2, 2] = 125
     with pytest.raises(ValueError, match="non binary"):
         brighten_areas_near_borders(img)
+
+
+def test_brighten_areas_near_borders_empty():
+    img = np.empty((0,), dtype=np.uint8)
+    assert (brighten_areas_near_borders(img) == img).all()
