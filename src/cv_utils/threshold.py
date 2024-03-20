cb203fc129638fbc789b9e6721cd22196481f83c
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-21 00:14:27 +0600

    style: 'gray' arg renamed to 'img'

--- src/cv_utils/threshold.py
+++ src/cv_utils/threshold.py
@@ -3,17 +3,17 @@ import numpy as np
 import numpy.typing as npt
 
 
-def binary(gray: npt.NDArray[np.uint8], thr: int) -> npt.NDArray[np.uint8]:
-    return cv2.threshold(gray, thr, 255, cv2.THRESH_BINARY)[1]
+def binary(img: npt.NDArray[np.uint8], thr: int) -> npt.NDArray[np.uint8]:
+    return cv2.threshold(img, thr, 255, cv2.THRESH_BINARY)[1]
 
 
-def inv_binary(gray: npt.NDArray[np.uint8], thr: int) -> npt.NDArray[np.uint8]:
-    return cv2.threshold(gray, thr, 255, cv2.THRESH_BINARY_INV)[1]
+def inv_binary(img: npt.NDArray[np.uint8], thr: int) -> npt.NDArray[np.uint8]:
+    return cv2.threshold(img, thr, 255, cv2.THRESH_BINARY_INV)[1]
 
 
-def otsu(gray: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
-    return cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+def otsu(img: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
 
 
-def inv_otsu(gray: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
-    return cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+def inv_otsu(img: npt.NDArray[np.uint8]) -> npt.NDArray[np.uint8]:
+    return cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
