be5323ae1beaa9ff31a83752d5d2359a4465da1a
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-20 21:43:59 +0600

    fix: fixed annotations, removed raise, added +1 to second coords

--- src/cv_utils/blobs.py
+++ src/cv_utils/blobs.py
@@ -4,11 +4,11 @@ import numpy.typing as npt
 from .coords import x1_y1_x2_y2
 
 
-def get_bright_rect(binary: npt.NDArray[np.uint8]) -> x1_y1_x2_y2:
+def get_bright_rect(binary: npt.NDArray[np.uint8 | np.bool_]) -> x1_y1_x2_y2 | None:
     nonzeros = binary.nonzero()
     try:
         y1, x1 = np.minimum.reduce(nonzeros, axis=1)
         y2, x2 = np.maximum.reduce(nonzeros, axis=1)
     except ValueError:
-        raise ValueError("Image is completely black")
-    return x1_y1_x2_y2(x1, y1, x2, y2)
+        return None
+    return x1_y1_x2_y2(x1, y1, x2 + 1, y2 + 1)
