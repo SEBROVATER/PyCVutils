2e54d1cef1c1233d30e31c4ee004aea1631aba17
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-22 00:10:16 +0600

    doc: added desctiptions

--- src/cv_utils/padding.py
+++ src/cv_utils/padding.py
@@ -4,10 +4,9 @@ import numpy.typing as npt
 
 
 def equal(
-        img: npt.NDArray[np.uint8],
-        value: int | tuple[int, ...],
-        size: int
+    img: npt.NDArray[np.uint8], value: int | tuple[int, ...], size: int
 ) -> npt.NDArray[np.uint8]:
+    """Pads each side by equal count of pixels"""
     return cv2.copyMakeBorder(
         img,
         top=size,
@@ -20,13 +19,20 @@ def equal(
 
 
 def unequal(
-        img: npt.NDArray[np.uint8],
-        value: int | tuple[int, ...],
-        top: int = 0,
-        bottom: int = 0,
-        left: int = 0,
-        right: int = 0,
+    img: npt.NDArray[np.uint8],
+    value: int | tuple[int, ...],
+    top: int = 0,
+    bottom: int = 0,
+    left: int = 0,
+    right: int = 0,
 ) -> npt.NDArray[np.uint8]:
+    """Pads each side by different count of pixels"""
     return cv2.copyMakeBorder(
-        img, top=top, bottom=bottom, left=left, right=right, borderType=cv2.BORDER_CONSTANT, value=value
+        img,
+        top=top,
+        bottom=bottom,
+        left=left,
+        right=right,
+        borderType=cv2.BORDER_CONSTANT,
+        value=value,
     )
