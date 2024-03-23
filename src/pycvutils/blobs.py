675f4cc56510d0f114ff94cca33ff95fed2390e4
SEBROVATER <sebrovskiy.k@gmail.com>
2024-03-23 16:51:37 +0600

    feat: added function 'get_all_borders'

--- src/pycvutils/blobs.py
+++ src/pycvutils/blobs.py
@@ -1,3 +1,5 @@
+from typing import Iterator
+
 import numpy as np
 import numpy.typing as npt
 
@@ -17,3 +19,16 @@ def get_bright_rect(binary: npt.NDArray[np.uint8 | np.bool_]) -> x1_y1_x2_y2 | N
     except ValueError:
         return None
     return x1_y1_x2_y2(x1, y1, x2 + 1, y2 + 1)
+
+
+def get_all_borders(bool_array: npt.NDArray[np.bool_]) -> Iterator[tuple[int, int]]:
+    """
+    Returns the coordinates for each blob in 1-dim array
+    """
+    a = np.r_[False, bool_array, False]
+    start = np.r_[False, ~a[:-1] & a[1:]]
+    end = np.r_[a[:-1] & ~a[1:], False]
+    for x1, x2 in zip(np.nonzero(start)[0] - 1, np.nonzero(end)[0]):
+        if x1 + 1 == x2:
+            continue
+        yield x1, x2
